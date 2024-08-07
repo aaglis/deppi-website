0a6405f98e88d8bb9b6691d604ecbd893dfb1f9a
aaglis <aglissilvafon@gmail.com>
2024-08-07 15:52:09 -0300

    feat(header): response with menu dropdown

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { ButtonLayoutComponent } from '../button-layout/button-layout.component';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ButtonLayoutComponent],
+  imports: [ButtonLayoutComponent, LucideAngularModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  switchImg: boolean = false
 
+  toggleBtn() {
+    this.switchImg = !this.switchImg
+  }
 }
