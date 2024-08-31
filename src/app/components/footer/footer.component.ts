66547a4f9e55cac9ac98a7fbe8b6c128955c62cb
aaglis <aglissilva17@gmail.com>
2024-08-31 13:49:22 -0300

    feat(landing page): implements accessibility

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -10,6 +10,13 @@ import { LucideAngularModule } from 'lucide-angular';
 })
 export class FooterComponent {
 
+  arrayLinks: {iconName: string, link: string}[] = [
+    {iconName: 'facebook', link: 'https://pt-br.facebook.com/ifcemaracanau'},
+    {iconName: 'linkedin', link: 'https://www.linkedin.com/in/deppi-ifce-maracana%C3%BA-183296215'},
+    {iconName: 'youtube', link: 'https://www.youtube.com/c/IFCEcampusdeMaracana%C3%BA'},
+    {iconName: 'instagram', link: 'https://www.instagram.com/ifcemaracanauoficial/?hl=pt-br'}
+  ]
+
   arrayIcons: string[] = ['facebook', 'linkedin', 'youtube', 'instagram'];
 
 }
